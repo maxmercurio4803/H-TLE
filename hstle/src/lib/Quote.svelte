fc853f00aba026961ef6bcd9ba67776d16f1afdb
maxmercurio4803 <max.mercurio4803@gmail.com>
2024-12-25 14:22:14 -0500

    revert

--- hstle/src/lib/Quote.svelte
+++ hstle/src/lib/Quote.svelte
@@ -7,7 +7,7 @@
 
     onMount(async () => {
         try {
-            const response = await fetch("https://hstle-bands.deno.dev/");
+            const response = await fetch("http://localhost:8000/");
 
             if (!response.ok) {
                 throw new Error("Failed to fetch quote");
